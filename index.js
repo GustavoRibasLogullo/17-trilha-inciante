0ffc8e055a5f20e89d5fe5ac40bfdce593989e80
GustavoRibasLogullo <gustavorlogullo@gmail.com>
2024-09-12 15:13:35 -0300

    Fundamentos 002

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-// Hello World
-console.log("hello world");
+let meta = {
+    value: 'ler um livro por mês',
+    checked: false,
+    log: (info) => {
+        console.log(info)
+    }
+}
+
+meta.log(meta.value)
+
+// arrow function
+// const criarMeta = () => {}
+// named function
+// function criarMeta() {}
